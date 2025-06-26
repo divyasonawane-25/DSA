b6c23464c0def4e19e2a512bcaaecd4b298b328f
Divya Rajesh Sonawane <ds25gamingtime@gmail.com>
2025-06-26 15:35:21 +0530

    Update doubletSum.cpp

--- doubletSum.cpp
+++ doubletSum.cpp
@@ -5,19 +5,19 @@ using namespace std;
 int main(){
   int x=10;
   vector<int> v(5);
+  vector<int> v1(2);
   cout<<"enter the values of array"<<endl;
-  
   for(int i=0;i<v.size();i++){
     cin>>v[i];
   }
 
-  for(int i=0;i<v.size();i++){
+  for(int i=0;i<5;i++){
 
-    for(int j=i+1;j>v.size();j++){
-      if(i+j==x ){
-         v[0]=i;
-         v[1]=j;
-         cout<<""
+    for(int j=i+1;j<5j++){
+      if(v[i]+v[j] == x ){
+         v1[0]=i;
+         v1[1]=j;
+         cout<<v1[0]<<" "<<v1[1];
       }
     }
     break;
